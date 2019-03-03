23bec31ed522fe3fa86064d58dbd7bdd9c28c423
Yi Chong Ong <yichong96@gmail.com>
2019-03-03 21:12:14 +0800

    fixed small errors

---
+++ src/main/java/seedu/address/logic/parser/ImportCommandParser.java
@@ -0,0 +1,15 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.ImportCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parser input for import command arguments.
+ */
+public class ImportCommandParser implements Parser<ImportCommand> {
+
+    @Override
+    public ImportCommand parse(String userInput) throws ParseException {
+        return null;
+    }
+}
