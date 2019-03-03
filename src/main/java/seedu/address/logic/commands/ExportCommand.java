12d94d98f58b208d1b57559be959bcf9513b34fd
Yi Chong Ong <yichong96@gmail.com>
2019-03-03 18:20:41 +0800

    added sort command base class

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -1,9 +1,12 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_FILENAME;
+
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_FILENAME;
 import seedu.address.model.Model;
+
+
 
 /**
  * Exports single or multiple card folders into a .json file. Users must specify file name to export card folders to.
@@ -16,7 +19,7 @@ public class ExportCommand extends Command {
             + "Parameters: CARD_FOLDER_NAME [MORE CARD_FOLDER_NAMES]"
             + PREFIX_FILENAME + "Filename\n"
             + "Example: " + COMMAND_WORD + "Human_anatomy Bone_structure n/myfilename";
-
+    public static final String MESSAGE_SUCCESS = "Successfully exported card folders to: $1%s";
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         return null;
