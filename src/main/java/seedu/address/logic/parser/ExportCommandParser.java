4da6e04f77b73854b4f1c200d6402b191e3b2bfd
Yi Chong Ong <yichong96@gmail.com>
2019-03-03 19:03:18 +0800

    added export command parser class

---
+++ src/main/java/seedu/address/logic/parser/ExportCommandParser.java
@@ -0,0 +1,15 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.ExportCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input for export arguments and creates a new export command object
+ */
+public class ExportCommandParser implements Parser<ExportCommand> {
+
+    @Override
+    public ExportCommand parse(String userInput) throws ParseException {
+        return null;
+    }
+}
