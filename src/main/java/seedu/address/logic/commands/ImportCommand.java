30dbf44d8dfb7996fc1707bdbe2caa470f6d8586
Yi Chong Ong <yichong96@gmail.com>
2019-03-03 17:52:07 +0800

    Changed import command usage

--- src/main/java/seedu/address/logic/commands/ImportCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -4,15 +4,19 @@ import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
+/**
+ * Imports a .json file containing card folders data into the application
+ */
 public class ImportCommand extends Command {
 
     public static final String COMMAND_WORD = "import";
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": imports a card folder into the application. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": imports a file containing card folders stored in "
+            + ".Json format to the application. "
             + "File imported must be in .json format.\n"
             + "Parameters: JSON_FILE_NAME\n "
-            + "Example: " + COMMAND_WORD + "Human_anatomy.json";
+            + "Example: " + COMMAND_WORD + "alice.json";
 
-    
+
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         return null;
